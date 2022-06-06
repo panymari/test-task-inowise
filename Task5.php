595fcc3a0729cfe0c3958d2c5a301621082452e0
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-06 20:24:23 +0300

    fix the task

--- Task5.php
+++ Task5.php
@@ -4,14 +4,38 @@ namespace src;
 
 class Task5
 {
+    public function fib(int $index): int
+    {
+        if ($index <= 1) {
+            return $index;
+        }
+
+        return $this->fib($index - 1) + $this->fib($index - 2);
+    }
+
     public function main(int $n): int
     {
-        $s = (int)(sqrt(5 * $n ** 2 + 4));
+        $index = 1;
+        $firstNumber = 0;
+        $secondNumber = 1;
 
-        if ($s ** 2 == 5 * $n ** 2 + 4 || $s ** 2 == 5 * $n ** 2 - 4) {
-            return $n;
-        } else {
-            return $this->main($n + 1);
+        while (true) {
+            $fibNum = $firstNumber + $secondNumber;
+            $index += 1;
+            $firstNumber = $secondNumber;
+            $secondNumber = $fibNum;
+
+            if (intval($fibNum / (10 ** ($n - 1))) > 0) {
+                break;
+            }
+
+            if ($secondNumber === $firstNumber && $secondNumber > 1) {
+                throw new \InvalidArgumentException("This result isn't reliable");
+            }
         }
+        return $this->fib($index);
     }
 }
+
+$t = new Task5();
+echo $t->main(10);
