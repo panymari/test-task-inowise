94a84b588c0a660343d6daef2a7634fbd4280e8a
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-06 13:29:57 +0300

    fix the structure

--- Task12.php
+++ Task12.php
@@ -2,19 +2,6 @@
 
 namespace src;
 
-//Write a PHP Calculator class which will accept two values as arguments,
-// then add them, subtract them, multiply them together, or divide them on request.
-//Provide an opportunity to make calculations by chain.
-//
-//For example:
-//
-//$mycalc = new MyCalculator(12, 6);
-//echo $mycalc->add(); // Displays 18
-//echo $mycalc->multiply(); // Displays 72
-//// Calculation by chain
-//echo $mycalc->add()->divideBy(9); // Displays 2 ( (12+6)/9=2 )
-
-
 class Task12
 {
     private float $value1;
