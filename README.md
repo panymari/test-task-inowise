dab04ad07fd942cafa5d4089c8a4ad4b83b4520d
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-06 15:39:50 +0300

    change the structure

---
+++ README.md
@@ -0,0 +1,5 @@
+Task 4. Given a string where words are separated by any of the '_', '-', ' ' characters. Write a function that converts such strings to single studly caps case words.
+For example
+Input:     The quick-brown_fox jumps over the_lazy-dog
+Output:    TheQuickBrownFoxJumpsOverTheLazyDog
+Func args:  (string $input)
--- Task4.php
+++ Task4.php
@@ -2,14 +2,6 @@
 
 namespace src;
 
-//Task 4. Given a string where words are separated by any of the '_', '-', ' '
-// characters. Write a function that converts such strings to
-// single studly caps case words.
-//For example
-//Input:     The quick-brown_fox jumps over the_lazy-dog
-//Output:    TheQuickBrownFoxJumpsOverTheLazyDog
-//Func args:  (string $input)
-
 class Task4
 {
     public function main(string $input): string
