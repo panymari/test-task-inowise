50ad4cce4bcc2ad348d63839793774d7bc4c6cc0
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-06 15:42:53 +0300

    change the structure

---
+++ README.md
@@ -0,0 +1,3 @@
+Task 2. Create a simple 'birthday countdown' script, the script counts the number of days left until the person’s birthday. Your script should determine the number of days based on the current date.
+Func args: (string $date).
+Acceptable date format is ’DD.MM.YYYY’
--- Task2.php
+++ Task2.php
@@ -2,12 +2,6 @@
 
 namespace src;
 
-//Task 2. Create a simple 'birthday countdown' script, the script
-// counts the number of days left until the person’s birthday. Your
-// script should determine the number of days based on the current date.
-//Func args: (string $date).
-//Acceptable date format is ’DD.MM.YYYY’
-
 class Task2
 {
     public function main(string $date): string
