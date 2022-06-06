668065f3eddaba806161eb83eec3e165c3c38de1
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-06 15:41:06 +0300

    change the structure

--- Task3.php
+++ Task3.php
@@ -2,14 +2,6 @@
 
 namespace src;
 
-//Task 3. Write a PHP program to add the digits by absolute of an integer
-// repeatedly until the result has a single digit.
-//
-//For example
-//Input: 5689
-//Output: 1
-//Explanation: 5689 = 5+6+8+9 = 28 = 2+8 = 10 = 1+0 = 1
-
 class Task3
 {
     public function main(int $value): int
