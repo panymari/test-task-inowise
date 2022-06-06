5a8db54a0dba0a340fb61c96bb2f7feca698194c
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-06 13:31:37 +0300

    fix the structure

---
+++ Task10.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace src;
+
+class Task10
+{
+    public function main(int $input): array
+    {
+        $resultArr[] = $input;
+        if ($input < 1) {
+            throw new \InvalidArgumentException("Input must be only positive value!");
+        }
+        while ($input > 1) {
+            if ($input % 2 === 0) {
+                $input = $input / 2;
+            } else {
+                $input = $input * 3 + 1;
+            }
+            array_push($resultArr, $input);
+        }
+        return $resultArr;
+    }
+}
