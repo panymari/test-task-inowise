eca15ab15beb7fb1ff8cd28ee40ef22ff77d43b7
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-06 13:34:26 +0300

    delete the task from the file

--- Task9.php
+++ Task9.php
@@ -2,21 +2,6 @@
 
 namespace src;
 
-//Write a PHP program to find three numbers from an array such that the sum of
-// three consecutive numbers equal to a given number.
-//Input:
-//
-//Array: [2, 7, 7, 1, 8, 2, 7, 8, 7]
-//Number: 16
-//Output:
-//Array
-//(
-//    [0] => 2 + 7 + 7 = 16
-//[1] => 7 + 1 + 8 = 16
-//)
-//
-//Func args: (array $arr, int $number)
-
 class Task9
 {
     public function main(array $arr, int $number): array
