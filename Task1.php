6eacd506fbb45624d34bed37680dacfe0b44a1c1
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-06 15:43:52 +0300

    change the structure

--- Task1.php
+++ Task1.php
@@ -2,16 +2,6 @@
 
 namespace src;
 
-//Task 1. Write a PHP function to test whether a number is greater than 30,
-// 20 or 10 using a ternary operator.
-//
-//Input args: (int $inputNumber)
-//Output args: Function must be return one value from list:
-//More than 30
-//More than 20
-//More than 10
-//Less than 10
-
 class Task1
 {
     public function main(int $inputNumber): string
