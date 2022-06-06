b39139530612657a3242d91aa0cf32b867c383f0
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-06 13:43:44 +0300

    change the structure

--- Task6.php
+++ Task6.php
@@ -2,17 +2,6 @@
 
 namespace src;
 
-//Write a function that finds how many Mondays occurred on the first day of each
-// month in the period from January 1, 1980 to December 31, 1999. Your function
-// should return Mondays count and the date when this Monday occurred and print
-// value to the console. Each value should be placed on the new line and dates should
-// have format DD.MM.YYYY. Try to implement the most optimized version of the function.
-//Output: count of Mondays
-//2
-//01.02.1980
-//01.03.1900
-//Func args: (int $year, int $lastYear, int $month, int $lastMonth, string $day = 'Monday')
-
 class Task6
 {
     public function getDaysInMonth($month, $year): int
