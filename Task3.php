31a5fd65de15d76204a22af00efea1ea1e0f6d86
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-07 23:20:55 +0300

    use php-cs-fixer

--- Task3.php
+++ Task3.php
@@ -11,12 +11,14 @@ class Task3
         if (count($digitsArr) > 1) {
             $sumOfDigits = array_reduce($digitsArr, function ($carry, $item) {
                 $carry += $item;
+
                 return $carry;
             });
             echo $this->main($sumOfDigits);
         } else {
             return intval($digitsArr);
         }
+
         return 0;
     }
 }
