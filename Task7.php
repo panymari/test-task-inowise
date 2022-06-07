ff53872d5a9c4dfd8ad86194bf26f4939f7040e5
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-07 23:27:58 +0300

    use php-cs-fixer

--- Task7.php
+++ Task7.php
@@ -7,9 +7,10 @@ class Task7
     public function main(array $arr, int $position): array
     {
         if (count($arr) <= $position) {
-            throw new \InvalidArgumentException("There is no such position here!");
+            throw new \InvalidArgumentException('There is no such position here!');
         }
         array_splice($arr, $position, 1);
+
         return $arr;
     }
 }
