a564b6f5a90e0017a6fdcf2384a1be5967f407cc
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-07 23:21:38 +0300

    use php-cs-fixer

--- Task4.php
+++ Task4.php
@@ -6,11 +6,13 @@ class Task4
 {
     public function main(string $input): string
     {
-        if (str_contains($input, "_") || str_contains($input, "-")) {
+        if (str_contains($input, '_') || str_contains($input, '-')) {
             $stringWithSpaces = ucwords(str_replace(['-', '_'], ' ', $input));
+
             return str_replace(' ', '', $stringWithSpaces);
         } else {
             $firstLetterUpperCaseString = ucwords($input);
+
             return str_replace(' ', '', $firstLetterUpperCaseString);
         }
     }
