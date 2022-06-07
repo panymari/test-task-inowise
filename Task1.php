84c80c3ac4a5b605a49908a394863aabf30ca67e
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-07 23:17:15 +0300

    use php-cs-fixer

--- Task1.php
+++ Task1.php
@@ -7,11 +7,11 @@ class Task1
     public function main(int $inputNumber): string
     {
         return $inputNumber > 30
-            ? "More than 30"
+            ? 'More than 30'
             : ($inputNumber > 20
-                ? "More than 20"
+                ? 'More than 20'
                 : ($inputNumber > 10
-                    ? "More than 30"
-                    : "Less than 10"));
+                    ? 'More than 30'
+                    : 'Less than 10'));
     }
 }
