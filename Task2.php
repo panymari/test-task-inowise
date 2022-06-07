398d2cd038ca2f943e15077e48a12e5b52712172
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-07 23:19:52 +0300

    use php-cs-fixer

--- Task2.php
+++ Task2.php
@@ -7,9 +7,9 @@ class Task2
     public function main(string $date): string
     {
         $dayMonth = substr($date, 0, 5);
-        $nextYearDate = $dayMonth . "." . date("Y") + 1;
+        $nextYearDate = $dayMonth . '.' . date('Y') + 1;
 
-        $currentDate = strtotime(date("d-m-Y"));
+        $currentDate = strtotime(date('d-m-Y'));
         $currentDateNextYear = strtotime($nextYearDate);
         $targetDate = strtotime($date);
 
