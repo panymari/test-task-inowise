3310ae0b3ccb687aab5e4d39ee5c90ee8f705df9
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-09 23:28:04 +0300

    fix the task

--- Task9.php
+++ Task9.php
@@ -6,15 +6,13 @@ class Task9
 {
     public function main(array $arr, int $number): array
     {
-        if ($number <= 0 || count($arr) === 2) {
+        if ($number === 0 || count($arr) <= 2) {
             throw new \InvalidArgumentException('Invalid input!');
         }
-        $j = 0;
         $result = [];
         for ($i = 0;$i < count($arr) - 2; $i++) {
             if ($arr[$i] + $arr[$i + 1] + $arr[$i + 2] === $number) {
-                $result = ["[$j]" => "{$arr[$i]} + {$arr[$i + 1]} + {$arr[$i + 2]} = 0"];
-                $j++;
+                array_push($result, ["{$arr[$i]} + {$arr[$i + 1]} + {$arr[$i + 2]} = $number"]);
             }
         }
 
