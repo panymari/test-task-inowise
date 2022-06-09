c1ef75cd06e7675716ce0db3eafc4f292a49c23a
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-09 16:51:03 +0300

    fix the task

--- Task8.php
+++ Task8.php
@@ -4,12 +4,13 @@ namespace src;
 
 class Task8
 {
-    public function main($json): object
+    public function main($json): array
     {
-        if (!is_string($json)) {
+        if (!is_string($json) || ctype_digit($json)) {
             throw new \InvalidArgumentException('Invalid input!');
         }
+        $j = json_decode($json, true);
 
-        return json_decode($json);
+        return array_merge($j);
     }
 }
