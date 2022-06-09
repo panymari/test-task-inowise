17371dbc78356e66df1d0efb25cf972f6227cf64
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-09 21:38:12 +0300

    fix the task

--- Task5.php
+++ Task5.php
@@ -4,6 +4,34 @@ namespace src;
 
 class Task5
 {
+    public function sum(string $a, string $b): int
+    {
+        $maxLength = max(strlen($a), strlen($b));
+
+        $a = str_pad($a, $maxLength, '0', STR_PAD_LEFT);
+        $b = str_pad($b, $maxLength, '0', STR_PAD_LEFT);
+
+        $result = '';
+
+        $buf = 0;
+
+        $arr = [];
+
+        for ($i = $maxLength - 1; $i >= 0; $i--) {
+            $sum = (int)$a[$i] + (int)$b[$i] + $buf;
+
+            $buf = 0;
+
+            if ($sum > 9) {
+                $buf = 1;
+                $sum = $sum % 10;
+            }
+
+            $arr[] = $sum.$result;
+        }
+
+        return intval(implode($arr)) ;
+    }
     public function main(int $n): string
     {
         if ($n <= 0) {
@@ -17,7 +45,11 @@ class Task5
         $n3 = 0;
 
         while (strlen(strval($n3)) !== $n) {
-            $n3 = $n1 + $n2;
+            if ($n1 > 2880067194370816120 && $n2 > 4660046610375530309) {
+                $n3 = $this->sum($n1, $n2);
+            } else {
+                $n3 = $n1 + $n2;
+            }
             $n1 = $n2;
             $n2 = $n3;
         }
