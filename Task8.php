3611c2763fcb10847e86de57945ac65a66071644
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-09 10:26:08 +0300

    use php-cs-fixer

--- Task8.php
+++ Task8.php
@@ -7,9 +7,9 @@ class Task8
     public function main($json): object
     {
         if (!is_string($json)) {
-            throw new \InvalidArgumentException("Invalid input!");
+            throw new \InvalidArgumentException('Invalid input!');
         }
+
         return json_decode($json);
     }
 }
-
