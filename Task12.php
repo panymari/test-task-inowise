1f7aaafe5dfded29cbb8cfdc16d5be8a313d629a
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-12 14:10:55 +0300

    fix the task

--- Task12.php
+++ Task12.php
@@ -6,7 +6,7 @@ class Task12
 {
     private int $value1;
     private int $value2;
-    protected string $result;
+    public int $result;
 
     public function __construct($value1, $value2)
     {
@@ -22,11 +22,6 @@ class Task12
     public function getValue2(): int
     {
         return $this->value2;
-    }
-
-    public function __toString()
-    {
-        return $this->result;
     }
 
     public function getResult(): int
@@ -57,7 +52,7 @@ class Task12
 
     public function divide(): Task12
     {
-        if ($this->getValue2() == 0) {
+        if ($this->getValue2() === 0) {
             throw new \InvalidArgumentException('Division by zero');
         }
         $this->result = $this->getValue1() / $this->getValue2();
@@ -65,48 +60,32 @@ class Task12
         return $this;
     }
 
-    public function divideBy(int $input): int
+    public function divideBy(int $input): Task12
     {
-        if ($input == 0) {
+        if ($input === 0) {
             throw new \InvalidArgumentException('Division by zero');
         }
 
-        if ($this->result == 0) {
-            return $input;
-        } else {
-            $this->result /= $input;
+        $this->result /= $input;
 
-            return $this->result;
-        }
+        return $this;
     }
-    public function multiplyBy(int $input): int
+    public function multiplyBy(int $input): Task12
     {
-        if ($this->result == 0) {
-            return $input;
-        } else {
-            $this->result *= $input;
+        $this->result *= $input;
 
-            return $this->result;
-        }
+        return $this;
     }
-    public function subtractBy(int $input): int
+    public function subtractBy(int $input): Task12
     {
-        if ($this->result == 0) {
-            return $input;
-        } else {
-            $this->result -= $input;
+        $this->result -= $input;
 
-            return $this->result;
-        }
+        return $this;
     }
-    public function addBy(int $input): int
+    public function addBy(int $input): Task12
     {
-        if ($this->result == 0) {
-            return $input;
-        } else {
-            $this->result += $input;
+        $this->result += $input;
 
-            return $this->result;
-        }
+        return $this;
     }
 }
