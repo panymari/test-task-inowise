6be0841a6e6ad16fa83fb789b5508768ace84edb
Maria Pashkovskaya <panymari84@gmail.com>
2022-06-12 17:52:46 +0300

    fix the task

--- Task8.php
+++ Task8.php
@@ -6,11 +6,11 @@ class Task8
 {
     public function main($json): string
     {
-        if (!is_string($json) || ctype_digit($json)) {
+        $obj = json_decode($json, true);
+
+        if (!$obj || !is_string($json) || ctype_digit($json)) {
             throw new \InvalidArgumentException('Invalid input!');
         }
-
-        $obj = json_decode($json, true);
 
         $arr = [];
         foreach ($obj as $item => $value) {
